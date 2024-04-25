3871be60594484881a448e2ae3b7ff307424510d
Maca Ruffa <101282037+MacarenaRuffa@users.noreply.github.com>
2024-04-25 19:42:39 -0300

    streams, lambda

--- Ejercicio1/src/main/java/org/example/banco/Main.java
+++ Ejercicio1/src/main/java/org/example/banco/Main.java
@@ -1,3 +1,17 @@
+/*
+Un banco tiene diferentes tipos de transacciones que puede llevar a cabo, entre ellas se encuentran:
+Depósito, Transferencia, Retiro de Efectivo, Consulta de Saldo y Pago de Servicios. Todas las transacciones
+tienen dos métodos en común, que son transaccionOk() y transaccionNoOk().
+A partir de esto existen diferentes tipos de clientes que pueden interactuar con el banco:
+Ejecutivos: Realizan Depósitos y Transferencias.
+Basico: Realizan consultas de saldo, pagos de servicios y retiro de efectivo.
+Cobradores: Realizan retiro de efectivo y consulta de saldos.
+Implementar el escenario según corresponda para permitir a los clientes mencionados con anterioridad,
+el acceso a los diferentes métodos según la operación que deseen realizar.
+Pista técnica: Para implementar los métodos en cada clase no es necesario realizar cálculos o funciones.
+Basta con simplemente colocar un mensaje por pantalla con la acción que se está realizando. Por ejemplo:
+System.out.println(“Depósito realizado correctamente”).
+*/
 package org.example.banco;
 
 public class Main {
--- Ejercicio1/src/main/java/org/example/fotocopiadora/Main.java
+++ Ejercicio1/src/main/java/org/example/fotocopiadora/Main.java
@@ -1,3 +1,16 @@
+/*
+Una fotocopiadora plantea desarrollar un programa que permita imprimir diferentes tipos de documentos.
+Entre los tipos de documentos se encuentran:
+Curriculums Atributos: nombrePersona, DNI, tituloUniversitario, años de experiencia
+Libros en PDF Atributos: cantidad de páginas, título, autor y género.
+Informes Atributos: Incluyen contenido, cantidad de páginas, autor, y revisor.
+Cada uno de estos documentos tienen en común el atributo id_documento y el método imprimir();
+sin embargo éste último es implementado de manera diferente en cada documento:
+En el caso de los curriculums, al llamar al método imprimir se debe mostrar cada uno de los atributos del objeto.
+En el caso de los libros, al llamar al método imprimir se debe mostrar título y autor.
+En el caso de los informes, al llamar al método imprimir sólo se debe mostrar el contenido.
+*/
+
 package org.example.fotocopiadora;
 
 public class Main {
